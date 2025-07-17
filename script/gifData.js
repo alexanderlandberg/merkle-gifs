7bf4348509bd62a81f561015cc2aaca70a14352c
alexanderlandberg <alexanderlandberg@users.noreply.github.com>
2025-07-17 12:26:13 +0200

    Added new gif "stu-stu-studio"

--- script/gifData.js
+++ script/gifData.js
@@ -1,112 +1,144 @@
-const gifDataArr = [{
-    "file": "tbone-schoolbus.gif",
-    "categories": ["custom"],
-    "people": ["thomas"]
-}, {
-    "file": "jim-wave2.gif",
-    "categories": ["reactions"],
-    "people": ["jimmi"]
-}, {
-    "file": "binoculars.gif",
-    "categories": ["custom"],
-    "people": ["thomas"]
-}, {
-    "file": "darth-vader.gif",
-    "categories": ["custom"],
-    "people": ["thomas"],
-    "tags": ["star wars"]
-}, {
-    "file": "diglett.gif",
-    "categories": ["custom"],
-    "people": ["thomas"],
-    "tags": ["pokémon"]
-}, {
-    "file": "eggs.gif",
-    "categories": ["custom"],
-    "people": ["thomas"]
-}, {
-    "file": "haystack.gif",
-    "categories": ["custom"],
-    "people": ["thomas"]
-}, {
-    "file": "hiding-bushes.gif",
-    "categories": ["custom"],
-    "people": ["thomas"]
-}, {
-    "file": "jamesbond-mask.gif",
-    "categories": ["custom"],
-    "people": ["thomas"]
-}, {
-    "file": "merkle-illuminati.gif",
-    "categories": ["custom"],
-    "tags": ["merkle"]
-}, {
-    "file": "metro-gm.gif",
-    "categories": ["custom"],
-    "people": ["thomas"]
-}, {
-    "file": "pennywise.gif",
-    "categories": ["custom"],
-    "people": ["thomas"],
-    "tags": ["pennywise"]
-}, {
-    "file": "pennywise2.gif",
-    "categories": ["custom"],
-    "people": ["thomas"],
-    "tags": ["pennywise"]
-}, {
-    "file": "red-pill_v2.gif",
-    "categories": ["custom"],
-    "people": ["thomas"],
-    "tags": ["matrix"]
-}, {
-    "file": "red-pill2_v2.gif",
-    "categories": ["custom"],
-    "people": ["thomas"],
-    "tags": ["matrix"]
-}, {
-    "file": "whos-that-pokemon.gif",
-    "categories": ["custom"],
-    "people": ["thomas"],
-    "tags": ["pokémon"]
-}, {
-    "file": "bobby.gif",
-    "categories": ["reactions"],
-    "people": ["bobby"]
-}, {
-    "file": "jim_clap.gif",
-    "categories": ["reactions"],
-    "people": ["jimmi"]
-}, {
-    "file": "jim_clap2.gif",
-    "categories": ["reactions"],
-    "people": ["jimmi"]
-}, {
-    "file": "jim_wave.gif",
-    "categories": ["reactions"],
-    "people": ["jimmi"]
-}, {
-    "file": "jim-wendy_clap.gif",
-    "categories": ["reactions"],
-    "people": ["jimmi", "wendy"]
-}, {
-    "file": "jim-wendy_wave.gif",
-    "categories": ["reactions"],
-    "people": ["jimmi", "wendy"]
-}, {
-    "file": "kamelasa.gif",
-    "categories": ["reactions"],
-    "tags": ["nordics"]
-}, {
-    "file": "SameSame.gif",
-    "categories": ["reactions"],
-    "people": ["jimmi"]
-}, {
-    "file": "tbone-scratch.gif",
-    "categories": ["reactions"],
-    "people": ["thomas"]
-}, {
-    "file": "tillykke.gif",
-    "categories": ["reactions"],
-    "people": ["kold", "laila"]
-}]
+const gifDataArr = [
+  {
+    file: "stu-stu-studio.gif",
+    categories: ["custom"],
+    tags: ["STUDIO"],
+  },
+  {
+    file: "tbone-schoolbus.gif",
+    categories: ["custom"],
+    people: ["thomas"],
+  },
+  {
+    file: "jim-wave2.gif",
+    categories: ["reactions"],
+    people: ["jimmi"],
+  },
+  {
+    file: "binoculars.gif",
+    categories: ["custom"],
+    people: ["thomas"],
+  },
+  {
+    file: "darth-vader.gif",
+    categories: ["custom"],
+    people: ["thomas"],
+    tags: ["star wars"],
+  },
+  {
+    file: "diglett.gif",
+    categories: ["custom"],
+    people: ["thomas"],
+    tags: ["pokémon"],
+  },
+  {
+    file: "eggs.gif",
+    categories: ["custom"],
+    people: ["thomas"],
+  },
+  {
+    file: "haystack.gif",
+    categories: ["custom"],
+    people: ["thomas"],
+  },
+  {
+    file: "hiding-bushes.gif",
+    categories: ["custom"],
+    people: ["thomas"],
+  },
+  {
+    file: "jamesbond-mask.gif",
+    categories: ["custom"],
+    people: ["thomas"],
+  },
+  {
+    file: "merkle-illuminati.gif",
+    categories: ["custom"],
+    tags: ["merkle"],
+  },
+  {
+    file: "metro-gm.gif",
+    categories: ["custom"],
+    people: ["thomas"],
+  },
+  {
+    file: "pennywise.gif",
+    categories: ["custom"],
+    people: ["thomas"],
+    tags: ["pennywise"],
+  },
+  {
+    file: "pennywise2.gif",
+    categories: ["custom"],
+    people: ["thomas"],
+    tags: ["pennywise"],
+  },
+  {
+    file: "red-pill_v2.gif",
+    categories: ["custom"],
+    people: ["thomas"],
+    tags: ["matrix"],
+  },
+  {
+    file: "red-pill2_v2.gif",
+    categories: ["custom"],
+    people: ["thomas"],
+    tags: ["matrix"],
+  },
+  {
+    file: "whos-that-pokemon.gif",
+    categories: ["custom"],
+    people: ["thomas"],
+    tags: ["pokémon"],
+  },
+  {
+    file: "bobby.gif",
+    categories: ["reactions"],
+    people: ["bobby"],
+  },
+  {
+    file: "jim_clap.gif",
+    categories: ["reactions"],
+    people: ["jimmi"],
+  },
+  {
+    file: "jim_clap2.gif",
+    categories: ["reactions"],
+    people: ["jimmi"],
+  },
+  {
+    file: "jim_wave.gif",
+    categories: ["reactions"],
+    people: ["jimmi"],
+  },
+  {
+    file: "jim-wendy_clap.gif",
+    categories: ["reactions"],
+    people: ["jimmi", "wendy"],
+  },
+  {
+    file: "jim-wendy_wave.gif",
+    categories: ["reactions"],
+    people: ["jimmi", "wendy"],
+  },
+  {
+    file: "kamelasa.gif",
+    categories: ["reactions"],
+    tags: ["nordics"],
+  },
+  {
+    file: "SameSame.gif",
+    categories: ["reactions"],
+    people: ["jimmi"],
+  },
+  {
+    file: "tbone-scratch.gif",
+    categories: ["reactions"],
+    people: ["thomas"],
+  },
+  {
+    file: "tillykke.gif",
+    categories: ["reactions"],
+    people: ["kold", "laila"],
+  },
+];
